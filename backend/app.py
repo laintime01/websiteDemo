c396b3b0f5b6fef4bb56d42ff28d29bbb92d5df8
laintime <hzhe111@163.com>
2022-12-09 16:29:23 +0800

    openai start

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,24 @@
 from flask import Flask
+import os
+import openai
 
+openai.api_key = os.getenv("OPENAI_API_KEY")
 app = Flask(__name__)
+
+response = openai.Completion.create(
+    model='text-davinci-001',
+    prompt='Write a tagline for an ice cream shop',
+    temperature=0.4,
+    max_tokens=64,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return response
 
 
 if __name__ == '__main__':
